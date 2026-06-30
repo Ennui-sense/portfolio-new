bf6c24efcf822fa3fb0ec086ba0badaa9d481eb4
Anatoliy Shreder <anatoliy.shreder@gmail.com>
2026-06-30 10:11:53 +0500

    add: 3d model

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,55 @@
 import "./Header.scss";
 
-import meImageSrc from "~/assets/images/me.png";
+import { Suspense } from "react";
+import { Bounds, Center, OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 import { stackData } from "~/data/stackData";
 
 import Socials from "../Socials/Socials";
 
+const PROFILE_MODEL_SRC = "/me.glb";
+
+const ProfileModel = () => {
+  const { scene } = useGLTF(PROFILE_MODEL_SRC);
+
+  return (
+    <Bounds fit clip observe margin={1.15}>
+      <Center>
+        <primitive object={scene} />
+      </Center>
+    </Bounds>
+  );
+};
+
+useGLTF.preload(PROFILE_MODEL_SRC);
+
 const Header = () => {
   return (
     <header className="header">
       <div className="header__inner container">
-        <img
-          src={meImageSrc}
-          alt="Мой профиль лица сгенерированный нейросетью"
+        <div
           className="header__image"
-          width={120}
-          height={120}
-        />
+          role="img"
+          aria-label="3D-модель профиля"
+        >
+          <Canvas camera={{ position: [0, 0.2, 4], fov: 35 }}>
+            <ambientLight intensity={1.4} />
+            <directionalLight position={[3, 4, 5]} intensity={2.2} />
+            <directionalLight position={[-3, 2, 2]} intensity={0.8} />
+
+            <Suspense fallback={null}>
+              <ProfileModel />
+            </Suspense>
+
+            <OrbitControls
+              enableDamping
+              enablePan={false}
+              enableZoom={false}
+              rotateSpeed={0.7}
+            />
+          </Canvas>
+        </div>
 
         <div className="header__content">
           <p className="header__name">Анатолий Шредер</p>
